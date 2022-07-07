5143eeedf597b37501925a79da67c59f4b9eed01
anthony3738-miZJgd <anthony3738@gmail.com>
2022-07-06 22:44:58 -0700

    Created initial question rendering

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,37 +1,51 @@
-
+// Defining HTML elements as variables
 var startButton = document.getElementById("startButton");
 var highScoreEl = document.getElementById("highScore");
-var startEl = document.getElementById("start");
-var questionEl = document.getElementById("question");
-var resultEl = document.getElementById("result");
+var startEl = document.getElementById("start-screen");
+var questionEl = document.getElementById("question-screen");
+var resultEl = document.getElementById("result-screen");
+var questionTitle = document.getElementById("questionTitle");
+var answersLi = document.getElementById("answers")
 
-var questionIndex = ""
-var quizTimer = questions.length * 20 
+// Initializing Variables
+var questionIndex = 0
+var quizTimer = questions.length * 20
 
-console.log({ resultEl })
-console.log(quizTimer)
-console.log(highScoreEl)
-console.log(questionEl)
-console.log(questions)
-
-
-// Removes the "hide" element from the quiz 
-// update class attribute to remove hide
-// str.replace("hide","");
+console.log(answersLi)
 
 // Function to start the quiz and initiate question rendering
 function startQuiz() {
     startEl.setAttribute("class", "hide");
-    questionEl.setAttribute("class","question")
+    questionEl.setAttribute("class", "show")
     questionRender()
+    startTimer()
 }
 
 
 // Renders the questions within the page
 function questionRender() {
+    var currentQ = questions[questionIndex]
+    questionEl.children[0].textContent = currentQ.question;
+    answerQty = currentQ.choices.length;
+    console.log(answerQty)
+
+    for (let i = 0; i < answerQty; i++) {
+        let qAnswer = currentQ.choices[i]
+        var choiceButton = document.createElement("button");
+        choiceButton.setAttribute("class","answer");
+        choiceButton.setAttribute("value",qAnswer);
+        choiceButton.textContent = qAnswer;
+        // console.log(qAnswer)
+        console.log(choiceButton)
+        answersLi.appendChild(choiceButton)
+    }
+
 }
 
-// Question Correctness Check
+startTimer()
+
+
+// Question Correctness Check/Cycles to next question
 function isCorrect() {
 
 }
