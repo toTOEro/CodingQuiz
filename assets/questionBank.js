413a0347d7e78f3817dea06bbf21610aa63535af
anthony3738-miZJgd <anthony3738@gmail.com>
2022-07-06 22:15:47 -0700

    Removed screen classes, updated to show or hide; added start button functionality

---
+++ assets/questionBank.js
@@ -0,0 +1,39 @@
+// Establishing question bank with an array of objects containing key value pairs
+var questions = [
+    {
+        id: "1",
+        question: "What is a string?",
+        choices: ["Thread", "Potatos", "A value that is contained within quotes (\")", "I DUNNO AHHH"],
+        answer: 3
+    },
+
+    {
+        id: "2",
+        question: "What does JS stand for?",
+        choices: ["Jungler Strength", "JavaScript", "Jesyln's Stand-o", "JorbaScropt"],
+        answer: 2
+    },
+
+    {
+        id: "3",
+        question: "What does CSS do?",
+        choices: ["Styles your HTML", "Styles your JavaScript", "Styles your hair", "Wins the lottery"],
+        answer: 1
+    },
+
+    {
+        id: "4",
+        question: "Are you having fun?",
+        choices: ["Yes", "No"],
+        answer: 1
+    },
+
+    {
+        id: "5",
+        question: "If I don't know where to find information... where should I go?",
+        choices: ["reddit.com", "noanswershere.com", "adidas.com", "google.com"],
+        answer: 4
+    }
+];
+
+// console.log({questions})
--- assets/quiz.js
+++ assets/quiz.js
@@ -1,18 +1,43 @@
 
-
+var startButton = document.getElementById("startButton");
 var highScoreEl = document.getElementById("highScore");
 var startEl = document.getElementById("start");
 var questionEl = document.getElementById("question");
 var resultEl = document.getElementById("result");
 
+var questionIndex = ""
+var quizTimer = questions.length * 20 
 
-
-console.log({resultEl})
-console.log(startEl)
+console.log({ resultEl })
+console.log(quizTimer)
 console.log(highScoreEl)
 console.log(questionEl)
+console.log(questions)
 
 
 // Removes the "hide" element from the quiz 
 // update class attribute to remove hide
 // str.replace("hide","");
+
+// Function to start the quiz and initiate question rendering
+function startQuiz() {
+    startEl.setAttribute("class", "hide");
+    questionEl.setAttribute("class","question")
+    questionRender()
+}
+
+
+// Renders the questions within the page
+function questionRender() {
+}
+
+// Question Correctness Check
+function isCorrect() {
+
+}
+
+
+startButton.addEventListener("click", startQuiz)
+
+
+
