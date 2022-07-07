a539b4ab5ee0e439f611d6b23be1e5f13706293f
anthony3738-miZJgd <anthony3738@gmail.com>
2022-07-06 21:43:55 -0700

    Created new JS files, generated basic styling + start screen styling. Added reset.css

---
+++ assets/quiz.js
@@ -0,0 +1,18 @@
+
+
+var highScoreEl = document.getElementById("highScore");
+var startEl = document.getElementById("start");
+var questionEl = document.getElementById("question");
+var resultEl = document.getElementById("result");
+
+
+
+console.log({resultEl})
+console.log(startEl)
+console.log(highScoreEl)
+console.log(questionEl)
+
+
+// Removes the "hide" element from the quiz 
+// update class attribute to remove hide
+// str.replace("hide","");
